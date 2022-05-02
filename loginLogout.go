7423bf4ae97d03f8763ff1f7035a88709848868e
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-05-03 04:34:37 +0800

    first commit

---
+++ loginLogout.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type user struct {
+	Username string
+	Password []byte
+	First    string
+	Last     string
+}
+
+type userCustomer struct {
+	Username string
+	Password []byte
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var mapUsers = map[string]user{}
+var mapUserCustomers = map[string]userCustomer{}
+var mapSessions = map[string]string{}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/loginCustomer", loginCustomer)
+	http.HandleFunc("/customerPage", customerPage)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe("127.0.0.1:5221", nil)
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	bPassword, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	mapUsers["admin"] = user{"admin", bPassword, "admin", "admin"}
+	mapUserCustomers["Azri"] = userCustomer{"azri", bPassword, "Azri", "rahmat"}
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	myUser := getUser(res, req)
+	tpl.ExecuteTemplate(res, "index.gohtml", myUser)
+}
+
+func signup(res http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	var myUser user
+	// process form submission
+	if req.Method == http.MethodPost {
+		// get form values
+		username := req.FormValue("username")
+		password := req.FormValue("password")
+		firstname := req.FormValue("firstname")
+		lastname := req.FormValue("lastname")
+		if username != "" {
+			if _, ok := mapUsers[username]; ok {
+				http.Error(res, "Username already taken", http.StatusForbidden)
+				return
+			}
+			id := uuid.NewV4()
+			myCookie := &http.Cookie{
+				Name:  "myCookie",
+				Value: id.String(),
+			}
+			http.SetCookie(res, myCookie)
+			mapSessions[myCookie.Value] = username
+
+			bPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+			if err != nil {
+				http.Error(res, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+			//store the hash in the memory, map the data in "myUser" then use this to talk the server once only
+			myUser = user{username, bPassword, firstname, lastname}
+			mapUsers[username] = myUser
+		}
+		// redirect to main index
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+
+	}
+	tpl.ExecuteTemplate(res, "signup.gohtml", myUser)
+}
+
+func login(res http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		username := req.FormValue("username")
+		password := req.FormValue("password")
+		// check if user exist with username
+		myUser, ok := mapUsers[username]
+		if !ok {
+			http.Error(res, "Username and/or password do not match", http.StatusUnauthorized)
+			return
+		}
+		// Matching of password entered
+		err := bcrypt.CompareHashAndPassword(myUser.Password, []byte(password))
+		if err != nil {
+			http.Error(res, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		// create session
+		id := uuid.NewV4()
+		myCookie := &http.Cookie{
+			Name:  "myCookie",
+			Value: id.String(),
+		}
+		http.SetCookie(res, myCookie)
+		mapSessions[myCookie.Value] = username
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(res, "login.gohtml", nil)
+}
+
+func loginCustomer(res http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		username := req.FormValue("username")
+		password := req.FormValue("password")
+		// check if user exist with username
+		myUser, ok := mapUsers[username]
+		if !ok {
+			http.Error(res, "Username and/or password do not match", http.StatusUnauthorized)
+			return
+		}
+		// Matching of password entered
+		err := bcrypt.CompareHashAndPassword(myUser.Password, []byte(password))
+		if err != nil {
+			http.Error(res, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		// create session
+		id := uuid.NewV4()
+		myCookie := &http.Cookie{
+			Name:  "myCookie",
+			Value: id.String(),
+		}
+		http.SetCookie(res, myCookie)
+		mapSessions[myCookie.Value] = username
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(res, "loginCustomer.gohtml", nil)
+}
+
+func customerPage(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func getUser(res http.ResponseWriter, req *http.Request) user {
+	myCookie, err := req.Cookie("myCookie")
+	if err != nil {
+		id := uuid.NewV4()
+		myCookie = &http.Cookie{
+			Name:  "myCookie",
+			Value: id.String(),
+		}
+	}
+
+	http.SetCookie(res, myCookie)
+
+	var myUser user
+
+	if username, ok := mapSessions[myCookie.Value]; ok {
+		myUser = mapUsers[username]
+	}
+	return myUser
+}
+
+func getUserCustomer(res http.ResponseWriter, req *http.Request) userCustomer {
+	myCookie, err := req.Cookie("myCookie")
+	if err != nil {
+		id := uuid.NewV4()
+		myCookie = &http.Cookie{
+			Name:  "myCookie",
+			Value: id.String(),
+		}
+	}
+
+	http.SetCookie(res, myCookie)
+
+	var myUserCustomer userCustomer
+
+	if username, ok := mapSessions[myCookie.Value]; ok {
+		myUserCustomer = mapUserCustomers[username]
+	}
+	return myUserCustomer
+}
+func alreadyLoggedIn(req *http.Request) bool {
+	myCookie, err := req.Cookie("myCookie")
+	if err != nil {
+		return false
+	}
+	username := mapSessions[myCookie.Value]
+	_, ok := mapUsers[username]
+	return ok
+}
